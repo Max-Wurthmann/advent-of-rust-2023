adaedc6feed1f56e1d4269a0877497e5d70f7f71
Max-Wurthmann <mx.wurthmann@gmail.com>
2024-09-19 15:08:27 +0200

    day4 done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use indoc::indoc;
 
 fn main() {}
@@ -195,4 +197,53 @@ fn is_symbol(c_opt: Option<char>) -> bool {
     let symbols = "$%&+-*#";
 
     symbols.contains(c)
+}
+
+#[test]
+fn day4test() {
+    let input = indoc! {"
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    "};
+
+    let expected = 13;
+    let result = day4(input);
+
+    assert_eq!(expected, result);
+}
+
+fn day4(scratch_cards: &str) -> i32 {
+    scratch_cards
+        .lines()
+        .map(|line| -> &str {
+            let (_, right) = line.split_once(':').unwrap();
+            right
+        })
+        .map(|line| line.split_once('|').unwrap())
+        .map(|(left, right)| {
+            let winning_numbers: HashSet<i32> = left
+                .split(' ')
+                .filter_map(|str| str.parse::<i32>().ok())
+                .collect();
+
+            let drawn_numbers = right.split(' ').filter_map(|str| str.parse::<i32>().ok());
+
+            let winning_count = drawn_numbers
+                .filter(|val| winning_numbers.contains(val))
+                .count();
+
+            winning_count
+        })
+        .map(|count| {
+            if count == 0 {
+                0
+            } else {
+                2_i32.pow((count - 1).try_into().unwrap())
+            }
+        })
+        .sum()
 }
